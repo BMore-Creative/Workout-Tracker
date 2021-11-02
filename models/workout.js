080862d282854dc7f7930cf5b8cf3dcde77537b4
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-11-01 22:24:35 -0700

    added files: mongoose index.js and workout model

---
+++ models/workout.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date(),
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: 'Please enter an exercise type',
+            },
+            name: {
+                type: Sting,
+                trim: true,
+                required: 'Please enter an exercise name',
+            },
+            duration: {
+                type: Number,
+                required: 'Please enter a duration for the exercise',
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            },
+        },
+    ],
+});
+
+const Workout = model("Workout", workoutSchema);
+
+module.exports = Workout;
