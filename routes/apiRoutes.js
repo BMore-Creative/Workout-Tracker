cdd3df42dd7c2caffae0450065022bef1840803c
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-11-02 06:29:54 -0700

    altered model/routes set up to accomodate seeder set up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/");
+const Workout = require("../models/workout");
 
 router.get("/workouts", async (req, res) => {
   try {
