00b3fcbe16dbc2c14ab6424a1ea70499a4765f5d
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-11-02 06:38:15 -0700

    altered server.js to accomodate for heroku deployment

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
