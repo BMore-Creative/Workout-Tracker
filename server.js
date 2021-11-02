3077cfaa0f26c6d0d35d98085e5b016159c44b39
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-11-02 06:44:04 -0700

    troubleshooting heroku deployment through server.js

--- server.js
+++ server.js
@@ -16,11 +16,14 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutDB',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(routes);
 
